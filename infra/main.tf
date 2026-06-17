2a140f8cc8163a1241747319d15345532724800f
Aiden25451 <a.asprakis@gmail.com>
2026-06-16 23:24:04 -0400

    Update to add s3 state management

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket = "piranesi-terraform-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+terraform {
   required_version = ">= 1.6.0"
 
   required_providers {
