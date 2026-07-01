a32da7cfc6af1d881d982a225cb19b3ae90c0ed7
Aiden25451 <a.asprakis@gmail.com>
2026-06-30 20:46:42 -0400

    Attempt 2 update thumbprint

--- terraform/bootstrap/iam.tf
+++ terraform/bootstrap/iam.tf
@@ -8,7 +8,7 @@ locals {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = ["9514F4ED3C841C96C43DEF0F0ACBF177405DED12"]
 }
 
 # IAM role for GitHub Actions OIDC assume
