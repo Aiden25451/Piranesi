c4ff4b32dd54fe435ebbc40af1aa2afc33e6505d
Aiden25451 <a.asprakis@gmail.com>
2026-06-30 20:59:36 -0400

    Please 2

--- terraform/bootstrap/iam.tf
+++ terraform/bootstrap/iam.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" : "repo:aasprakis/Piranesi*"
+            "token.actions.githubusercontent.com:sub" : "repo:aasprakis*/Piranesi*"
           }
         }
       }
